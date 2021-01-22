bfe6d2849de741b2ff90c95ae9725198f17d9c73
Michaël Perrin <contact@michaelperrin.fr>
2021-01-21 21:25:42 -0600

    Init components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import ItemList from './components/ItemList';
 import './App.css';
 
 function App() {
   return (
     <div className="container-fluid moving-list">
-      Hello
+      <ItemList />
     </div>
   );
 }
---
+++ src/components/ItemList/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Item from '../Item';
+
+const ItemList = () => (
+  <div>
+    <Item />
+  </div>
+);
+
+export default ItemList;
